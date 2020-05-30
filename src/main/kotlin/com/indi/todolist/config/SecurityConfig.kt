66defb9c920a46a7758a06eda25e6660fbfda04f
insukChoi <cis1725@gmail.com>
2020-05-31 01:35:58 +0900

    시큐리티 임시 제거

--- src/main/kotlin/com/indi/todolist/config/SecurityConfig.kt
+++ src/main/kotlin/com/indi/todolist/config/SecurityConfig.kt
@@ -1,21 +1,20 @@
-package com.indi.todolist.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-class SecurityConfig : WebSecurityConfigurerAdapter() {
-    override fun configure(http: HttpSecurity) {
-        http.authorizeRequests()
-                .antMatchers(
-                        "/swagger-resources/**",
-                        "/swagger-ui.html",
-                        "/v2/api-docs",
-                        "/webjars/**",
-                        "/h2-console/**",
-                        "/todo/**"  // TODO: 로그인 구현하면 빼야함
-                ).permitAll()
-                    .antMatchers("/**/*").denyAll();
-    }
-}
+package com.indi.todolist.config//package com.indi.todolist.config
+//
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//class SecurityConfig : WebSecurityConfigurerAdapter() {
+//    override fun configure(http: HttpSecurity) {
+//        http.authorizeRequests()
+//                .antMatchers(
+//                        "/swagger-ui.html",
+//                        "/v2/api-docs",
+//                        "/webjars/**",
+//                        "/h2-console/**",
+//                        "/todo/**"  // TODO: 로그인 구현하면 빼야함
+//                ).permitAll()
+//                .antMatchers("/**/*").permitAll();
+//    }
+//}
