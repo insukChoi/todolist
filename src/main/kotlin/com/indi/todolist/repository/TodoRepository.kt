d04ee8561057dc0a9944ae888581d7a3b6233427
insukChoi <cis1725@gmail.com>
2020-05-31 00:23:07 +0900

    안쓰는 import 삭제

--- src/main/kotlin/com/indi/todolist/repository/TodoRepository.kt
+++ src/main/kotlin/com/indi/todolist/repository/TodoRepository.kt
@@ -2,6 +2,5 @@ package com.indi.todolist.repository
 
 import com.indi.todolist.support.entity.Todo
 import org.springframework.data.jpa.repository.JpaRepository
-import java.awt.print.Pageable
 
 interface TodoRepository : JpaRepository<Todo, Long>
