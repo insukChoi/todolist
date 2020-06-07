9bf5629b947a8b05e7da6861e6e16cf287a5c829
insukChoi <cis1725@gmail.com>
2020-06-07 21:15:17 +0900

    화면단 구성

--- README.md
+++ README.md
@@ -4,7 +4,22 @@ TODO LIST
 - http://localhost:8088
 - http://localhost:8088/h2-console (DB 관리콘솔)
 - http://localhost:8088/swagger-ui.html (Swagger)
+- http://localhost:3000 (front)
 
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:3000
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
 ## 라이브러리 및 오픈소스
 - Kotlin
---
+++ src/ui/components/TodoInput.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" placeholder="할일이 무엇입니까"
+           v-on:keyup.enter="addTodo">
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"/>
+    </span>
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">warning</h3>
+      <h4 slot="body">할 일을 입력하세요.</h4>
+      <span slot="footer" @click="showModal = false">
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
+  </div>
+</template>
+
+<script>
+    import Modal from './common/Modal'
+    export default {
+      data() {
+        return {
+          newTodoItem: '',
+          showModal: false
+        }
+      },
+      methods: {
+        addTodo() {
+          if (this.newTodoItem !== ""){
+            const value = this.newTodoItem && this.newTodoItem.trim();
+            this.$emit('addTodo', value);
+          } else {
+            this.showModal = !this.showModal
+          }
+        }
+      },
+      components: {
+        Modal: Modal
+      }
+    }
+</script>
+
+<style scoped>
+  input:focus {
+    outline: none;
+  }
+  .inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+  }
+  .inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+  }
+  .addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: inline-block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+  }
+  .addBtn {
+    color: white;
+    vertical-align: middle;
+  }
+</style>
